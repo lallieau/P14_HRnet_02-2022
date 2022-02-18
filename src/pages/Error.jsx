ab481e152d7d00e5d745e73d7baaf4cd5c1ddb37
Lallie <lallie.audry@gmail.com>
2022-02-18 17:03:52 +0100

    create error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,3 +1,46 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+/**
+ * CSS for the component using styled.components
+ */
+const ErrorWrapper = styled.main`
+  align-items: center;
+  color: ${({ theme }) => theme.colors.primary};
+  display: flex;
+  flex-direction: column;
+  min-height: 85vh;
+  h1 {
+    font-size: clamp(6rem, 10vw, 15rem);
+    font-weight: 700;
+    margin: 6rem 0rem;
+  }
+  p {
+    font-size: clamp(1.125rem, 2vw, 2rem);
+    font-weight: 500;
+    margin-bottom: 5rem;
+    text-align: center;
+  }
+`;
+
+const ReturnLink = styled(Link)`
+  color: ${({ theme }) => theme.colors.primary};
+  font-size: clamp(0.875rem, 1.5vw, 1.375rem);
+  font-weight: 500;
+  margin-bottom: 2rem;
+`;
+
+/**
+ * Component to render Error 404 page
+ * @function Error
+ * @returns {JSX}
+ */
 export const Error = () => {
-  return <h1>Erreur</h1>;
+  return (
+    <ErrorWrapper>
+      <h1>404</h1>
+      <p>Oops! The page you requested does not exist</p>
+      <ReturnLink to="/">Return to the home page</ReturnLink>
+    </ErrorWrapper>
+  );
 };
