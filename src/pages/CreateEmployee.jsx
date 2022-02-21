a3b521140e5ff4e23483abc0d199813eaa456e1c
Lallie <lallie.audry@gmail.com>
2022-02-21 17:47:15 +0100

    use p14-plugin lib to implement modal

--- src/pages/CreateEmployee.jsx
+++ src/pages/CreateEmployee.jsx
@@ -2,7 +2,8 @@ import styled from 'styled-components';
 import { EmployeeForm } from '../components/EmployeeForm';
 import { Layout } from '../components/Layout';
 import { useState } from 'react';
-import { Modal } from '../components/Modal';
+import { Modal } from 'p14-plugin/dist';
+import done from '../assets/sammy-done.png';
 
 const Title = styled.h1`
   margin-bottom: 26px;
@@ -14,7 +15,13 @@ export const CreateEmployee = () => {
   return (
     <Layout title="HRnet | Create Employee">
       <Title>Create Employee</Title>
-      {modalIsOpen && <Modal setModalIsOpen={setModalIsOpen} />}
+      <Modal
+        show={modalIsOpen}
+        setShow={setModalIsOpen}
+        title="Success !"
+        text="The new employee has been created"
+        image={done}
+      />
       <EmployeeForm setModalIsOpen={setModalIsOpen} />
     </Layout>
   );
